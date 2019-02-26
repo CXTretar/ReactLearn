a1a2a44ab43ce4c09ea18db5a3375a0c24495e2d
CXTretar <misscxuan@163.com>
2019-02-26 09:37:59 +0800

    RNLearnDemo项目 修复安卓物理返回键Bug.

--- React Native/RNLearnDemo/js/common/BackPressComponent.js
+++ React Native/RNLearnDemo/js/common/BackPressComponent.js
@@ -11,14 +11,14 @@ export default class BackPressComponent {
     }
 
     componentDidMount() {
-        if (this.props.backPress) BackHandler.addEventListener('hardwareBackPress');
+        if (this.props.backPress) BackHandler.addEventListener('hardwareBackPress', this._hardwareBackPress);
     }
 
     componentWillUnmount() {
-        if (this.props.backPress) BackHandler.removeEventListener('hardwareBackPress');
+        if (this.props.backPress) BackHandler.removeEventListener('hardwareBackPress', this._hardwareBackPress);
     }
 
     onHardwareBackPress(e) {
-       return this.props.backPress(e);
+        return this.props.backPress(e);
     }
 }
